5909e096e9011d5391fcbf1abf14cd21234a1ed5
Holovko Maksym <golovkomaksim852@gmail.com>
2024-06-20 17:10:19 +0300

    all

--- src/generics/2.ts
+++ src/generics/2.ts
@@ -1,15 +1,32 @@
-// type AllType = {
-//   name: string;
-//   position: number;
-//   color: string;
-//   weight: number;
-// };
+type AllType = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number;
+};
 
-// function compare(top, bottom): AllType {
-//   return {
-//     name: top.name,
-//     color: top.color,
-//     position: bottom.position,
-//     weight: bottom.weight,
-//   };
-// }
+type TopType = Pick<AllType, "name" | "color">;
+type BottomType = Pick<AllType, "position" | "weight">;
+
+function compare(top: TopType, bottom: BottomType): AllType {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  };
+}
+
+const topItem: TopType = {
+  name: "cube",
+  color: "red",
+};
+
+const bottomItem: BottomType = {
+  position: 800,
+  weight: 400,
+};
+
+const result = compare(topItem, bottomItem);
+
+console.log(result);
